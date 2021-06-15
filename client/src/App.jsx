8afe10137ae3a3cf135bfcb013524aac4aec1690
Geozedeque Guimarães <geozedeque@outlook.com.br>
2021-06-15 16:55:45 -0300

    Card QuemSomos done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import {
-  PostList,
+  PostList, CardQuemSomos,
 } from './components';
+
+import LogoMissao from ".\\assets\\logo-missao.png";
 
 function App() {
   return (
-    <PostList />
+    <CardQuemSomos titulo="Missão" imagem={LogoMissao} descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
   );
 }
 
