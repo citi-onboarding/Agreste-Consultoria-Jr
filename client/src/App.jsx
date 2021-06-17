a253ba7db34a223ea3a0fc85227fbefb0aaf918a
Geozedeque Guimarães <geozedeque@outlook.com.br>
2021-06-17 16:23:44 -0300

    fix prop button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
 import {
-  PostList, 
-  CardQuemSomos,
   Button,
 } from './components';
 
-import LogoMissao from "../src/assets/logo-missao.png";
-
 function App() {
   return (
-    <CardQuemSomos titulo="Missão" imagem={LogoMissao} descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
-    <Button nomeBtn="Já volto" imagem='none' buttonTextColor="white" />
+  <Button nomeBtn="Mais sobre nós" buttonTextColor="#fff"/>
   );
 }
 
